7167af99d6ae12bf40abf39dae1be052bbf827e0
chonchol <chonchol57@gmail.com>
2025-02-19 00:04:00 +0600

    updated employee card design

--- src/pages/EmployeeListCard.jsx
+++ src/pages/EmployeeListCard.jsx
@@ -1,7 +1,18 @@
-import React from "react";
-
+import EmployeeCard from "../components/EmployeeCard";
 const EmployeeListCard = () => {
-  return <div>Hello</div>;
+  return (
+    <>
+      <div className="p-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        <EmployeeCard />
+        <EmployeeCard />
+        <EmployeeCard />
+        <EmployeeCard />
+        <EmployeeCard />
+        <EmployeeCard />
+        <EmployeeCard />
+      </div>
+    </>
+  );
 };
 
 export default EmployeeListCard;

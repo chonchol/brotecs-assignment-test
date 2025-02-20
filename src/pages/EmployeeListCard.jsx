87ccc93a9f3fe0adaffc276bee0236ed4380ecca
chonchol <chonchol57@gmail.com>
2025-02-20 12:17:53 +0600

    fix light mode issue

--- src/pages/EmployeeListCard.jsx
+++ src/pages/EmployeeListCard.jsx
@@ -2,14 +2,19 @@ import EmployeeCard from "../components/EmployeeCard";
 const EmployeeListCard = () => {
   return (
     <>
-      <div className="p-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        <EmployeeCard />
-        <EmployeeCard />
-        <EmployeeCard />
-        <EmployeeCard />
-        <EmployeeCard />
-        <EmployeeCard />
-        <EmployeeCard />
+      <div className="container grid px-6 mx-auto">
+        <h4 className="my-4 text-lg font-semibold text-gray-600 dark:text-gray-200">
+          Employee List
+        </h4>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+          <EmployeeCard />
+          <EmployeeCard />
+          <EmployeeCard />
+          <EmployeeCard />
+          <EmployeeCard />
+          <EmployeeCard />
+          <EmployeeCard />
+        </div>
       </div>
     </>
   );
